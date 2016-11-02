d6618f5947de6eb52c352d976f60a22050525517
Nicolas AZARI <nicodinh@azari.fr>
2016-11-02 05:03:34 +0000

    implemented methods: family

--- server.js
+++ server.js
@@ -9,12 +9,25 @@ server.connection({
   port: Config.server.port
 });
 
+server.method(require('./lib/pony').methods);
 server.register(require('./lib/plugins').plugins);
 server.route(require('./lib/routes').routes);
 
 server.start((err) => {
+
   if (err) {
     throw err;
   }
+
   server.log('info', 'Server running at: ' + server.info.uri);
+
+  server.methods.pony.family((err, result) => {
+    if (err) {
+      server.log('error', err);
+    }
+    if (result) {
+      server.log('info', result);
+    }  
+  });
+
 });
