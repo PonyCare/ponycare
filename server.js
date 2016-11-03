cc4929eaef119f34b7497683916f3296ee5f8ec7
Nicolas AZARI <nicodinh@azari.fr>
2016-11-03 00:04:53 +0000

    refa server.js

--- server.js
+++ server.js
@@ -14,20 +14,8 @@ server.register(require('./lib/plugins').plugins);
 server.route(require('./lib/routes').routes);
 
 server.start((err) => {
-
   if (err) {
     throw err;
   }
-
   server.log('info', 'Server running at: ' + server.info.uri);
-
-  server.methods.pony.family((err, result) => {
-    if (err) {
-      server.log('error', err);
-    }
-    if (result) {
-      server.log('info', result);
-    }  
-  });
-
 });
